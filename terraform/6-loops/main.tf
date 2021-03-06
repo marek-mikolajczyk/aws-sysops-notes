8afd0463b2ccf42be0b6ec51c5a20c08a81751a4
Your Name <you@example.com>
2021-03-06 01:09:40 +0100

    update

---
+++ terraform/6-loops/main.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "example" {
+	count = 3
+	name = "neo${count.index}"
+}
+
+
+variable "user_list" {
+	type = list
+	default = ["tom", "jerry"]
+}
+
+resource "aws_iam_user" "example2" {
+        count = length(var.user_list)
+        name = var.user_list[count.index]
+}
